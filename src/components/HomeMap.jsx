214e67b6b22fb3b73967764bf6fbe86f62994092
sandocsol <jungan1030@gmail.com>
2025-08-14 17:59:04 +0900

    style: prettier 적용

--- src/components/HomeMap.jsx
+++ src/components/HomeMap.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
 const MapContainer = styled.div`
   /* 핵심: 한쪽만 지정 + aspect-ratio 로 다른 축을 자동 계산 */
@@ -17,27 +17,25 @@ const MapContainer = styled.div`
 `;
 
 export default function HomeMap() {
-    useEffect(() => {
-        // 카카오맵 API가 로드될 때까지 기다리는 함수
-        const initMap = () => {
-            if (window.kakao && window.kakao.maps) {
-                const container = document.getElementById('map');
-                const options = {
-                    center: new window.kakao.maps.LatLng(37.5721, 126.9854),
-                    level: 10
-                };
-                // eslint-disable-next-line no-unused-vars
-                const map = new window.kakao.maps.Map(container, options);
-            } else {
-                // API가 아직 로드되지 않았다면 잠시 후 다시 시도
-                setTimeout(initMap, 100);
-            }
+  useEffect(() => {
+    // 카카오맵 API가 로드될 때까지 기다리는 함수
+    const initMap = () => {
+      if (window.kakao && window.kakao.maps) {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(37.5721, 126.9854),
+          level: 10,
         };
+        // eslint-disable-next-line no-unused-vars
+        const map = new window.kakao.maps.Map(container, options);
+      } else {
+        // API가 아직 로드되지 않았다면 잠시 후 다시 시도
+        setTimeout(initMap, 100);
+      }
+    };
 
-        initMap();
-    }, []);
-    
-  return (
-    <MapContainer id="map"></MapContainer>
-  )
+    initMap();
+  }, []);
+
+  return <MapContainer id="map"></MapContainer>;
 }
